b04c63563f15888d3267cabf3cf1f9bcbb9ad75d
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-10-01 14:02:40 +0300

    fix cards_solution

--- composition/cards/cards_solution/src/App.js
+++ composition/cards/cards_solution/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import CardOne from './components/CardOne';
 import CardTwo from './components/CardTwo';
+import Card from './components/Card';
 
 function App() {
+  const itemsOne = {
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni id accusamus voluptatem nam autem excepturi dolores totam quisquam.',
+    title: 'Hello, React!',
+    style: {width: '18rem'}
+  }
+
+  const itemsTow = {
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quidem. Sequi harum atque alias placeat hic pariatur quis voluptas, perferendis quibusdam est aliquam dolor quidem ea deserunt culpa maxime assumenda!',
+    title: 'Special Offer',
+    style: {width: '18rem'}
+  }
+
   return (
     <div className="App">
-      <CardOne />
+      <CardOne style={itemsOne.style}>
+        <img src="https://www.filepicker.io/api/file/SVFQZQAyRpqJ31f6LNGe" className="card-img-top" alt="React-logo" />
+        <Card text={itemsOne.text} title={itemsOne.title}/>
+      </CardOne>
       <br />
-      <CardTwo />
+      <CardTwo style={itemsTow.style}>
+        <Card text={itemsTow.text} title={itemsTow.title}/>
+      </CardTwo>
     </div>
   );
 }
--- composition/cards/cards_solution/src/components/CardTwo.js
+++ composition/cards/cards_solution/src/components/CardTwo.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import Card from './Card';
 
 function CardTwo(props) {
-    const text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quidem. Sequi harum atque alias placeat hic pariatur quis voluptas, perferendis quibusdam est aliquam dolor quidem ea deserunt culpa maxime assumenda!';
-    const title = 'Special Offer'
     return (
-        <div className="card" style={{width: '18rem'}}>
-            <Card text={text} title={title}/> 
+        <div className="card" style={props.style}>
+            {props.children}
         </div>
     )
 }
