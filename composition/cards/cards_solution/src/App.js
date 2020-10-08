063d66c52beda90906e03a8df80a57d7ca6a9379
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2020-10-08 14:11:13 +0300

    card repair

--- composition/cards/cards_solution/src/App.js
+++ composition/cards/cards_solution/src/App.js
@@ -7,25 +7,24 @@ function App() {
   const itemsOne = {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni id accusamus voluptatem nam autem excepturi dolores totam quisquam.',
     title: 'Hello, React!',
-    style: {width: '18rem'}
+    style: {width: '18rem'},
+    img: 'https://www.filepicker.io/api/file/SVFQZQAyRpqJ31f6LNGe',
+    link: 'google.com'
   }
 
   const itemsTow = {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quidem. Sequi harum atque alias placeat hic pariatur quis voluptas, perferendis quibusdam est aliquam dolor quidem ea deserunt culpa maxime assumenda!',
     title: 'Special Offer',
-    style: {width: '18rem'}
+    style: {width: '18rem'},
+    link: 'google.com'
   }
 
   return (
     <div className="App">
-      <CardOne style={itemsOne.style}>
-        <img src="https://www.filepicker.io/api/file/SVFQZQAyRpqJ31f6LNGe" className="card-img-top" alt="React-logo" />
-        <Card text={itemsOne.text} title={itemsOne.title}/>
-      </CardOne>
-      <br />
-      <CardTwo style={itemsTow.style}>
-        <Card text={itemsTow.text} title={itemsTow.title}/>
-      </CardTwo>
+      <Card link={itemsOne} text={itemsOne.text} title={itemsOne.title} style={itemsOne.style}>
+        <img src={itemsOne.img} className="card-img-top" alt="React-logo" />
+      </Card>
+      <Card link={itemsTow.link} text={itemsTow.text} title={itemsTow.title} style={itemsTow.style} />
     </div>
   );
 }
--- composition/cards/cards_solution/src/components/Card.js
+++ composition/cards/cards_solution/src/components/Card.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types'
 
 function Card(props) {
     return (
-        <div className="card-body">
-            <h5 className="card-title">{props.title ? props.title : undefined}</h5>
-            <p className="card-text">
-                {props.text ? props.text : undefined}
-            </p>
-            <a href="google.com" className="btn btn-primary">Go somewhere</a>
+        <div className="card" style={props.style}>
+            <div className="card-body">
+                <h5 className="card-title">{props.title ? props.title : null}</h5>
+                {props.children}
+                {props.text ? <p className="card-text">{props.text}</p> : null}
+                <a href={props.link} className="btn btn-primary">Go somewhere</a>
+            </div>
         </div>
     )
 }
